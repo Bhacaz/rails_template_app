fc1d185bb51c43b4cf75055b4b4235a6e256edf8
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-10-28 20:16:27 -0400

    rename generator

---
+++ lib/generators/rename/rename_generator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class RenameGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  def reset_credentials
+    remove_dir('config/credentials')
+    remove_file('config/credentials.yml.enc')
+    remove_file('config/master.key')
+    rails_command('credentials:edit')
+    rails_command('credentials:edit --environment production')
+  end
+
+  def rename_app
+    gsub_file('config/application.rb', 'RailsTemplateApp', class_name)
+  end
+end
